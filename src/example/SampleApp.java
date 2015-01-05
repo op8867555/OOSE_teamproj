ca830866ff9d671be765898760211ff3d9a59b3e
OriharaRanya <nightarashi@gmail.com>
2015-01-06 00:03:05 +0800

    UI

--- src/example/SampleApp.java
+++ src/example/SampleApp.java
@@ -50,32 +50,3 @@ class Seq extends ScreenGenerator {
             return EndScreen.getInstance();
     }
 }
-
-class Answers extends AnswerType {
-
-    ArrayList<Integer> as = new ArrayList<Integer>();
-    static Scanner s = new Scanner(System.in);
-    @Override
-    public void addOption(Object o) {
-        as.add((Integer) o);
-    }
-
-    public Integer prompt() throws IOException {
-        System.out.println("options:");
-        IntStream.range(0, this.as.size()).forEach(idx ->
-                System.out.println(idx + ". " + as.get(idx)));
-        int choice = s.nextInt();
-        Integer ans = as.get(choice);
-        as = new ArrayList<Integer>();
-        return ans;
-    }
-
-    @Override
-    public Object answer() {
-        try {
-            return prompt();
-        } catch (Exception e){
-            return 0;
-        }
-    }
-}
